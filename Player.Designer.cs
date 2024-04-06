ad8a17148f22a253b206935b24701e9aa00569e6
Xenatronics <sarah13310@proton.me>
2024-04-07 01:22:16 +0200

    version 0.1

--- Player.Designer.cs
+++ Player.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace XenPlayer
 {
-    partial class Form1
+    partial class Player
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,43 +29,85 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Player));
             ListViewItem listViewItem1 = new ListViewItem("Chanson1", 0);
             ListViewItem listViewItem2 = new ListViewItem("Chanson2", 1);
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             panelRight = new Panel();
+            panelCmd = new Panel();
+            btnFilr = new Button();
+            btnDel = new Button();
+            btnDelete = new Button();
             listView1 = new ListView();
             imageList1 = new ImageList(components);
             groupBox1 = new GroupBox();
-            button1 = new Button();
+            volumeCtl1 = new XenLibrary.VolumeCtl();
             lblMediaType = new Label();
             lblDuration = new Label();
             lblTitle = new Label();
-            volumeCtl1 = new XenLibrary.VolumeCtl();
             axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
-            panel2 = new Panel();
-            playerControl1 = new XenLibrary.PlayerControl();
+            playerCtl1 = new XenLibrary.PlayerCtl();
+            panelBar = new Panel();
             timer1 = new System.Windows.Forms.Timer(components);
             panelFront = new Panel();
-            button2 = new Button();
-            panel1 = new Panel();
+            openFileDialog1 = new OpenFileDialog();
             panelRight.SuspendLayout();
+            panelCmd.SuspendLayout();
             groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)axWindowsMediaPlayer1).BeginInit();
-            panel2.SuspendLayout();
+            panelBar.SuspendLayout();
             panelFront.SuspendLayout();
-            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // panelRight
             // 
             panelRight.BackColor = SystemColors.GradientActiveCaption;
-            panelRight.Controls.Add(panel1);
+            panelRight.Controls.Add(panelCmd);
             panelRight.Controls.Add(listView1);
             panelRight.Dock = DockStyle.Right;
             panelRight.Location = new Point(936, 0);
             panelRight.Name = "panelRight";
             panelRight.Size = new Size(500, 722);
             panelRight.TabIndex = 0;
+            // 
+            // panelCmd
+            // 
+            panelCmd.BackColor = SystemColors.GradientInactiveCaption;
+            panelCmd.Controls.Add(btnFilr);
+            panelCmd.Controls.Add(btnDel);
+            panelCmd.Controls.Add(btnDelete);
+            panelCmd.Dock = DockStyle.Top;
+            panelCmd.Location = new Point(0, 0);
+            panelCmd.Name = "panelCmd";
+            panelCmd.Size = new Size(500, 175);
+            panelCmd.TabIndex = 5;
+            // 
+            // btnFilr
+            // 
+            btnFilr.Image = (Image)resources.GetObject("btnFilr.Image");
+            btnFilr.Location = new Point(397, 52);
+            btnFilr.Name = "btnFilr";
+            btnFilr.Size = new Size(49, 40);
+            btnFilr.TabIndex = 5;
+            btnFilr.UseVisualStyleBackColor = true;
+            btnFilr.Click += btnFile_Click;
+            // 
+            // btnDel
+            // 
+            btnDel.Location = new Point(54, 108);
+            btnDel.Name = "btnDel";
+            btnDel.Size = new Size(131, 40);
+            btnDel.TabIndex = 3;
+            btnDel.Text = "Supprimer";
+            btnDel.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(54, 52);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(131, 40);
+            btnDelete.TabIndex = 4;
+            btnDelete.Text = "Ajouter";
+            btnDelete.UseVisualStyleBackColor = true;
             // 
             // listView1
             // 
@@ -93,26 +135,33 @@
             // groupBox1
             // 
             groupBox1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            groupBox1.Controls.Add(volumeCtl1);
             groupBox1.Controls.Add(lblMediaType);
             groupBox1.Controls.Add(lblDuration);
             groupBox1.Controls.Add(lblTitle);
+            groupBox1.Controls.Add(axWindowsMediaPlayer1);
             groupBox1.Dock = DockStyle.Fill;
             groupBox1.Location = new Point(0, 0);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(936, 630);
+            groupBox1.Size = new Size(936, 666);
             groupBox1.TabIndex = 3;
             groupBox1.TabStop = false;
             groupBox1.Text = "Informations média";
             // 
-            // button1
+            // volumeCtl1
             // 
-            button1.Location = new Point(54, 108);
-            button1.Name = "button1";
-            button1.Size = new Size(131, 40);
-            button1.TabIndex = 3;
-            button1.Text = "Supprimer";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            volumeCtl1.BorderStyle = BorderStyle.FixedSingle;
+            volumeCtl1.Location = new Point(871, 470);
+            volumeCtl1.MaximumSize = new Size(65, 195);
+            volumeCtl1.MinimumSize = new Size(65, 192);
+            volumeCtl1.Name = "volumeCtl1";
+            volumeCtl1.Size = new Size(65, 195);
+            volumeCtl1.TabIndex = 4;
+            volumeCtl1.TrackBarValue = 0;
+            volumeCtl1.Visible = false;
+            volumeCtl1.EventVolume += volumeCtl1_EventVolume;
+            volumeCtl1.Load += volumeCtl1_Load;
+            volumeCtl1.MouseLeave += volumeCtl1_MouseLeave;
             // 
             // lblMediaType
             // 
@@ -141,60 +190,46 @@
             lblTitle.TabIndex = 0;
             lblTitle.Text = "Titre";
             // 
-            // volumeCtl1
-            // 
-            volumeCtl1.Location = new Point(207, 465);
-            volumeCtl1.MaximumSize = new Size(65, 195);
-            volumeCtl1.MinimumSize = new Size(65, 192);
-            volumeCtl1.Name = "volumeCtl1";
-            volumeCtl1.Size = new Size(65, 195);
-            volumeCtl1.TabIndex = 4;
-            volumeCtl1.TrackBarValue = 0;
-            volumeCtl1.Visible = false;
-            volumeCtl1.EventVolume += volumeCtl1_EventVolume;
-            volumeCtl1.MouseLeave += volumeCtl1_MouseLeave;
-            // 
             // axWindowsMediaPlayer1
             // 
+            axWindowsMediaPlayer1.Dock = DockStyle.Bottom;
             axWindowsMediaPlayer1.Enabled = true;
-            axWindowsMediaPlayer1.Location = new Point(274, 168);
+            axWindowsMediaPlayer1.Location = new Point(3, 220);
             axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
             axWindowsMediaPlayer1.OcxState = (AxHost.State)resources.GetObject("axWindowsMediaPlayer1.OcxState");
-            axWindowsMediaPlayer1.Size = new Size(264, 23);
+            axWindowsMediaPlayer1.Size = new Size(930, 443);
             axWindowsMediaPlayer1.TabIndex = 2;
             axWindowsMediaPlayer1.Visible = false;
             // 
-            // panel2
+            // playerCtl1
             // 
-            panel2.BackColor = SystemColors.AppWorkspace;
-            panel2.Controls.Add(playerControl1);
-            panel2.Controls.Add(axWindowsMediaPlayer1);
-            panel2.Dock = DockStyle.Bottom;
-            panel2.Location = new Point(0, 630);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(936, 92);
-            panel2.TabIndex = 1;
+            playerCtl1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            playerCtl1.BorderStyle = BorderStyle.FixedSingle;
+            playerCtl1.Dock = DockStyle.Bottom;
+            playerCtl1.DurationText = "00:00 / 00:00";
+            playerCtl1.ForcePlay = false;
+            playerCtl1.Location = new Point(5, 5);
+            playerCtl1.Margin = new Padding(0);
+            playerCtl1.MinimumSize = new Size(600, 46);
+            playerCtl1.Name = "playerCtl1";
+            playerCtl1.Size = new Size(926, 46);
+            playerCtl1.TabIndex = 3;
+            playerCtl1.TrackBarMaximum = 10;
+            playerCtl1.TrackBarValue = 0;
+            playerCtl1.PlayPauseEvent += playerCtl1_PlayPauseEvent;
+            playerCtl1.VolumeEvent += playerControl1_VolumeEvent;
             // 
-            // playerControl1
+            // panelBar
             // 
-            playerControl1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            playerControl1.BorderStyle = BorderStyle.FixedSingle;
-            playerControl1.Dock = DockStyle.Fill;
-            playerControl1.DurationText = "00:00 / 00:00";
-            playerControl1.Location = new Point(0, 0);
-            playerControl1.Margin = new Padding(8);
-            playerControl1.MinimumSize = new Size(600, 92);
-            playerControl1.Name = "playerControl1";
-            playerControl1.Padding = new Padding(10, 12, 10, 12);
-            playerControl1.Size = new Size(936, 92);
-            playerControl1.TabIndex = 3;
-            playerControl1.TrackBarMaximum = 10;
-            playerControl1.TrackBarValue = 0;
-            playerControl1.FileSelected += playerControl1_FileSelected;
-            playerControl1.PlayEvent += playerControl1_PlayEvent;
-            playerControl1.PauseEvent += playerControl1_PauseEvent;
-            playerControl1.StopEvent += playerControl1_StopEvent;
-            playerControl1.VolumeEvent += playerControl1_VolumeEvent;
+            panelBar.AutoSize = true;
+            panelBar.BackColor = SystemColors.AppWorkspace;
+            panelBar.Controls.Add(playerCtl1);
+            panelBar.Dock = DockStyle.Bottom;
+            panelBar.Location = new Point(0, 666);
+            panelBar.Name = "panelBar";
+            panelBar.Padding = new Padding(5);
+            panelBar.Size = new Size(936, 56);
+            panelBar.TabIndex = 1;
             // 
             // panelFront
             // 
@@ -203,48 +238,32 @@
             panelFront.Dock = DockStyle.Fill;
             panelFront.Location = new Point(0, 0);
             panelFront.Name = "panelFront";
-            panelFront.Size = new Size(936, 630);
+            panelFront.Size = new Size(936, 666);
             panelFront.TabIndex = 1;
             // 
-            // button2
+            // openFileDialog1
             // 
-            button2.Location = new Point(54, 52);
-            button2.Name = "button2";
-            button2.Size = new Size(131, 40);
-            button2.TabIndex = 4;
-            button2.Text = "Ajouter";
-            button2.UseVisualStyleBackColor = true;
+            openFileDialog1.FileName = "openFileDialog1";
             // 
-            // panel1
-            // 
-            panel1.BackColor = SystemColors.GradientInactiveCaption;
-            panel1.Controls.Add(button1);
-            panel1.Controls.Add(button2);
-            panel1.Dock = DockStyle.Top;
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(500, 175);
-            panel1.TabIndex = 5;
-            // 
-            // Form1
+            // Player
             // 
             AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1436, 722);
             Controls.Add(panelFront);
-            Controls.Add(volumeCtl1);
-            Controls.Add(panel2);
+            Controls.Add(panelBar);
             Controls.Add(panelRight);
-            Name = "Form1";
+            Name = "Player";
             Text = "XenPlayer";
             panelRight.ResumeLayout(false);
+            panelCmd.ResumeLayout(false);
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)axWindowsMediaPlayer1).EndInit();
-            panel2.ResumeLayout(false);
+            panelBar.ResumeLayout(false);
             panelFront.ResumeLayout(false);
-            panel1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -257,13 +276,15 @@
         private Label lblMediaType;
         private XenLibrary.VolumeCtl volumeCtl1;
         private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
-        private Panel panel2;
-        private XenLibrary.PlayerControl playerControl1;
+        private Panel panelBar;
         private System.Windows.Forms.Timer timer1;
-        private Button button1;
+        private Button btnDel;
         private Panel panelFront;
         private ImageList imageList1;
-        private Button button2;
-        private Panel panel1;
+        private Button btnDelete;
+        private Panel panelCmd;
+        private XenLibrary.PlayerCtl playerCtl1;
+        private Button btnFilr;
+        private OpenFileDialog openFileDialog1;
     }
 }
--- Player.cs
+++ Player.cs
@@ -1,16 +1,20 @@
 using AxWMPLib;
+using System.Windows.Forms;
 using WMPLib;
 
 namespace XenPlayer
 {
-    public partial class Form1 : Form
+    public partial class Player : Form
     {
         double duration;
         int minutesT;
         int secondsT;
-        public Form1()
+        bool bMediaOpen = false;
+        public Player()
         {
             InitializeComponent();
+            this.axWindowsMediaPlayer1.uiMode = "none";
+            this.axWindowsMediaPlayer1.Visible = false;
             this.axWindowsMediaPlayer1.OpenStateChange += new AxWMPLib._WMPOCXEvents_OpenStateChangeEventHandler(axWindowsMediaPlayer1_OpenStateChange);
             InitializeTrackBarTimer();
         }
@@ -26,22 +30,24 @@ namespace XenPlayer
             if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
             {
                 int progressDuration = (int)axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
-                playerControl1.TrackBarValue = progressDuration;
-                playerControl1.DurationText = showDuration(progressDuration);
+                playerCtl1.TrackBarValue = progressDuration;
+                playerCtl1.DurationText = showDuration(progressDuration);
 
             }
             else if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsStopped)
             {
                 // Réinitialiser le TrackBar quand la lecture est arrêtée
-                playerControl1.TrackBarValue = 0;
+                playerCtl1.TrackBarValue = 0;
+                playerCtl1.ForcePlay = true;
+                playerCtl1.DurationText = showDuration(0);
             }
         }
         private void trackBar1_MouseCaptureChanged(object sender, EventArgs e)
         {
             // Si l'utilisateur relâche le clic de souris sur le TrackBar, changez la position de la lecture
-            if (!playerControl1.Capture)
+            if (!playerCtl1.Capture)
             {
-                axWindowsMediaPlayer1.Ctlcontrols.currentPosition = playerControl1.TrackBarValue;
+                axWindowsMediaPlayer1.Ctlcontrols.currentPosition = playerCtl1.TrackBarValue;
             }
         }
         private void axWindowsMediaPlayer1_OpenStateChange(object sender, _WMPOCXEvents_OpenStateChangeEvent e)
@@ -54,18 +60,19 @@ namespace XenPlayer
                 lblTitle.Text = "Titre : " + Path.GetFileName(axWindowsMediaPlayer1.URL); ;
                 IWMPMedia mediaInfo = axWindowsMediaPlayer1.currentMedia;
                 lblMediaType.Text = "Type : " + mediaInfo.getItemInfo("FileType").ToUpper();
-                playerControl1.DurationText = showDuration(0);
+                playerCtl1.DurationText = showDuration(0);
                 volumeCtl1.TrackBarValue = axWindowsMediaPlayer1.settings.volume;
                 // voir si la durée n'est pas zéro
                 if (duration > 0)
                 {
                     // Mettez à jour la valeur maximale du TrackBar ici
-                    playerControl1.TrackBarMaximum = (int)duration;
+                    playerCtl1.TrackBarMaximum = (int)duration;
                     minutesT = (int)duration / 60;
                     secondsT = (int)duration % 60;
-
+                    bMediaOpen = true;
+                    ;
                 }
-                playerControl1.TrackBarValue = 0;
+                playerCtl1.TrackBarValue = 0;
             }
         }
 
@@ -77,37 +84,10 @@ namespace XenPlayer
 
         private void btnPlay_Click(object sender, EventArgs e)
         {
-
             if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
             {
                 axWindowsMediaPlayer1.Ctlcontrols.pause();
             }
-        }
-
-        private void playerControl1_PlayEvent(object sender, EventArgs e)
-        {
-            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPaused || axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsStopped)
-            {
-                axWindowsMediaPlayer1.Ctlcontrols.play();
-                timer1.Start();
-            }
-        }
-
-        private void playerControl1_PauseEvent(object sender, EventArgs e)
-        {
-            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
-            {
-                axWindowsMediaPlayer1.Ctlcontrols.pause();
-                timer1.Stop();
-            }
-        }
-
-        private void playerControl1_StopEvent(object sender, EventArgs e)
-        {
-            axWindowsMediaPlayer1.Ctlcontrols.stop();
-            timer1.Stop();
-            playerControl1.TrackBarValue = 0;
-            playerControl1.DurationText = showDuration(0);
         }
 
         private void playerControl1_VolumeEvent(object sender, EventArgs e)
@@ -117,21 +97,6 @@ namespace XenPlayer
 
 
 
-        private void playerControl1_FileSelected(object sender, string e)
-        {
-            try
-            {
-                axWindowsMediaPlayer1.URL = e;
-                timer1.Stop();
-                timer1.Start();
-
-                axWindowsMediaPlayer1.Ctlcontrols.play(); // Jouer le média
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Erreur lors de la lecture du fichier : {ex.Message}", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
 
         private string calculDuration(double duration)
         {
@@ -165,9 +130,47 @@ namespace XenPlayer
             axWindowsMediaPlayer1.settings.volume = e;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void volumeCtl1_Load(object sender, EventArgs e)
         {
 
+        }
+
+       
+        private void playerCtl1_PlayPauseEvent(object sender, bool isPlay)
+        {
+            if (bMediaOpen == false)
+            {
+                playerCtl1.ForcePlay = true;
+                return;
+            }
+
+            if (isPlay)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.pause();
+            }
+            else
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+                {
+                    axWindowsMediaPlayer1.URL = openFileDialog1.FileName;
+                    timer1.Stop();
+                    timer1.Start();
+
+                    axWindowsMediaPlayer1.Ctlcontrols.play(); // Jouer le média
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur lors de la lecture du fichier : {ex.Message}", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
